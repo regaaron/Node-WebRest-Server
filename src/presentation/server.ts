6ab6d5baf24469e221868cb1680770ae499799e1
aaron lopez <aaron_lopez222@hotmail.com>
2026-05-04 18:24:42 -0600

    Fin de la Seccion 14 RestServer - CRUD

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'node:path';
 
 interface Options{
     PORT: number;
     PUBLIC_PATH: string;
+    routes: Router;
 }
 export class Server {
 
@@ -12,18 +13,33 @@ export class Server {
     private readonly port: number;
     private readonly public_path: string;
 
+    private readonly router: Router;
+
     constructor(option:Options){
         this.port = option.PORT;
         this.public_path = option.PUBLIC_PATH;
+        this.router = option.routes;
     }
 
     async start() {
 
+
+        //Midelwares
+        this.app.use( express.json());
+        this.app.use( express.urlencoded({ extended: true })); // x-www-form-urlencoded
+
+
+        //Public folder
         this.app.use(express.static(this.public_path));
+
+        //Routes
+        this.app.use(this.router);
 
         // this.app.use((req, res) => {
         //     res.sendFile(path.join(process.cwd(), "public", "index.html"));
         // });
+
+
         this.app.listen(this.port, () => {
             console.log(`Server running on Port ${this.port}`);
 
---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,93 @@
+import { Request, Response } from "express"
+import { todo } from "node:test"
+
+const todos = [
+    { id: 1, text: 'Buy milk', createdAt: new Date() },
+    { id: 2, text: 'Buy bread', createdAt: null },
+    { id: 3, text: 'Buy butter', createdAt: new Date() },
+
+] 
+
+
+
+export class TodosController {
+
+    //DI
+    constructor() {
+
+    }
+
+    public getTodos = (req: Request, res: Response) => {
+        return res.json(todos)
+    }
+
+    public getTodoById = (req:Request,res:Response) =>{
+
+        const id = +req.params.id!;
+        if(isNaN(id)) {
+            return res.status(400).json({ error: 'ID must be a number' });
+        }
+
+        const todo = todos.find(todo => todo.id === id);
+
+        if (!todo) {
+            return res.status(404).json({ error: `Todo with id '${id}' not found` });
+        }
+        res.json(todo);
+        
+    }
+
+    public createTodo = (req:Request,res:Response) =>{
+        const { text } = req.body;
+        if ( !text ) return res.status(400).json({ error: 'Text property is required' });
+
+        const newTodo = {
+            id: todos.length + 1,
+            text: text,
+            createdAt: null
+        };
+        todos.push(newTodo);
+        res.status(201).json(newTodo);
+    }
+
+    public updateTodo = (req:Request,res:Response) =>{
+        const id = +req.params.id!;
+        if(isNaN(id)) {
+            return res.status(400).json({ error: 'ID must be a number' });
+        }
+
+        const todo = todos.find(todo => todo.id === id);
+        if(!todo) {
+            return res.status(404).json({ error: `Todo with id '${id}' not found` });
+        }
+
+        const {text,createAt} = req.body;
+        if(!text) return res.status(400).json({ error: 'Text property is required' });
+
+        todo.text = text;
+        (createAt === "null") 
+        ? todo.createdAt = null 
+        : todo.createdAt = new Date(createAt || todo.createdAt);
+
+        res.json(todo);
+    }
+
+    public deleteTodo = (req:Request,res:Response) =>{
+        const id = +req.params.id!;
+
+        if(isNaN(id)) {
+            return res.status(400).json({ error: 'ID must be a number' });
+        }
+
+        const todoIndex = todos.findIndex(todo => todo.id === id);
+        if(todoIndex === -1) {
+            return res.status(404).json({ error: `Todo with id '${id}' not found` });
+        }
+
+        todos.splice(todoIndex, 1);
+        res.json({ message: `Todo with id '${id}' deleted` });
+    }
+    
+
+
+}
