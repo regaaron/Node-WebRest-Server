4164179f3765667755f638e74aca09579938c633
aaron lopez <aaron_lopez222@hotmail.com>
2026-05-16 01:20:54 -0600

    feat: implement TodoDatasource with CRUD methods and enhance TodoEntity formObject validation

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -13,6 +13,23 @@ export class TodoEntity {
         return !!this.completedAt;
     }
 
+    public static formObject(object: {[key: string]: any}): TodoEntity{
+        const {id,text,completedAt} = object
+        if(!id) throw new Error('ID is required');
+        if(!text) throw new Error('Text is required');
+        
+        let newCommpletedAt;
+        if(completedAt){
+            newCommpletedAt = new Date(completedAt);
+            if(isNaN(newCommpletedAt.getTime())){
+                throw new Error('CompletedAt is not a valid date');
+            }
+        }
+
+
+        return new TodoEntity(id, text, newCommpletedAt);
+
+    }
     
 
 }
---
+++ src/domain/index.ts
@@ -0,0 +1,4 @@
+export * from './datasources/todo.datasources';
+export * from './repositories/todo.repository';
+export * from './dtos';
+export * from './entities/todo.entity';
